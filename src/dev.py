9dafc1c212b9882b1c2409cf3a0fffe000563129
Mike Chen <chintown@gmail.com>
2012-11-19 12:29:33 +0800

    flask update

--- src/dev.py
+++ src/dev.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 import warnings
 
+
 def deprecated(func):
     """This is a decorator which can be used to mark functions
     as deprecated. It will result in a warning being emitted
